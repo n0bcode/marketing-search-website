15d0fc49824d13330aa3148c05f8aef40294ddf3
Trực Trần Trung <tructtpk03625@gmail.com>
2025-05-16 00:15:21 +0700

    chore(position): Change ResponseAPI and ExceptionHandler classes position for improved structure repo

--- backend-webAPI/Api/Utils/ExceptionHandler.cs
+++ backend-webAPI/Api/Utils/ExceptionHandler.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Constants;
+using Api.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Api.Constants
+namespace Api.Utils
 {
     public class ExceptionHandler
     {
